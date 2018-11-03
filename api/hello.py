11b5e83e5f32029f1f670a1b6ce5b2e62a5034f5
KenRatliff <kjhsfjk@yahoo.com>
2018-11-03 16:39:37 -0500

    Added stuff

--- api/hello.py
+++ api/hello.py
@@ -1,11 +1,18 @@
 from selen_help import *
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+
 
 def main():
-    driver = webdriver.Firefox()
-    driver.get("https://accessuh.uh.edu")
-    find_element(driver, By.ID, "param1").send_keys("bchu3")
-    find_element(driver, By.ID, "param2").send_keys("not_my_password")
-    find_element(driver, By.XPATH, "/html/body/main/div/section/div[2]/form/div/div[1]/i/input").click()
+
+    binary = FirefoxBinary(r'C:\Program Files\Mozilla Firefox\firefox.exe')
+    driver = webdriver.Firefox(firefox_binary=binary)
+    driver.get("https://www.expedia.com/")
+    find_element(driver, By.ID, "package-origin-hp-package").send_keys("houston")
+    find_element(driver, By.ID, "package-destination-hp-package").send_keys("atlanta")
+    find_element(driver, By.ID, "package-returning-hp-package").send_keys("12/3/2018")
+    find_element(driver, By.ID, "package-departing-hp-package").send_keys("11/3/2018")
+    #find_element(driver, By.XPATH, "/html/body/main/div/section/div[2]/form/div/div[1]/i/input").click()
 
 if __name__ == "__main__":
     main()
