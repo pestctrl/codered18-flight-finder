0c5521ac7ae5c5ea8b5461796c7f760506d5421a
Unknown <flurfle@gmail.com>
2018-11-04 00:21:08 -0500

    Points too, and zoom

--- FlightFinder/ClientApp/src/components/Counter.js
+++ FlightFinder/ClientApp/src/components/Counter.js
@@ -7,6 +7,10 @@ export class Counter extends Component {
             lat: 59.95,
             lng: 30.33
         },
+        coord: [
+            { lat: 53.42728, lng: -6.24357 },
+            { lat: 43.681583, lng: -79.61146 }
+        ],
         zoom: 11
     };
 
@@ -20,22 +24,41 @@ export class Counter extends Component {
                     defaultZoom={this.props.zoom}
                     onGoogleApiLoaded={({ map, maps }) => this.renderPolylines(map, maps)}
                 >
-                    
                 </GoogleMapReact>
             </div>
         );
     }
+
+    getMapBounds(map, maps, places) {
+        const bounds = new maps.LatLngBounds();
+
+        places.forEach((place) => {
+            bounds.extend(new maps.LatLng(
+                place.lat,
+                place.lng,
+            ));
+        });
+        return bounds;
+    }
     renderPolylines(map, maps) {
+        const bounds = this.getMapBounds(map, maps, this.props.coord);
+        // Fit map to bounds
+        map.fitBounds(bounds);
+
         let geodesicPolyline = new maps.Polyline({
-            path: [
-                { lat: 53.42728, lng: -6.24357 },
-                { lat: 43.681583, lng: -79.61146 }
-            ],
+            path: this.props.coord,
             geodesic: true,
             strokeColor: '#00a1e1',
             strokeOpacity: 1.0,
             strokeWeight: 4
         });
+        for (let i = 0; i < this.props.coord.length; i++) {
+            var marker = new maps.Marker({
+                position: this.props.coord[i],
+                map: map,
+                title: 'Hello World!'
+            });
+        }
         geodesicPolyline.setMap(map);
     }
 }
