d87607fb26399fa047a9a4d8ea31ca9e129f2cc8
Unknown <flurfle@gmail.com>
2018-11-03 23:52:25 -0500

    CURVY LINES

--- FlightFinder/ClientApp/src/components/Counter.js
+++ FlightFinder/ClientApp/src/components/Counter.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
 export class Counter extends Component {
-  displayName = Counter.name
+    static defaultProps = {
+        center: {
+            lat: 59.95,
+            lng: 30.33
+        },
+        zoom: 11
+    };
 
-  constructor(props) {
-    super(props);
-    this.state = { currentCount: 0 };
-    this.incrementCounter = this.incrementCounter.bind(this);
-  }
-
-  incrementCounter() {
-    this.setState({
-      currentCount: this.state.currentCount + 1
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Counter</h1>
-
-        <p>This is a simple example of a React component.</p>
-
-        <p>Current count: <strong>{this.state.currentCount}</strong></p>
-
-        <button onClick={this.incrementCounter}>Increment</button>
-      </div>
-    );
-  }
+    render() {
+        return (
+            // Important! Always set the container height explicitly
+            <div style={{ height: '100vh', width: '100%' }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: 'AIzaSyBKLPbrmfqp6_7vlPuibIBeWFYbIlGAnJI' }}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                    onGoogleApiLoaded={({ map, maps }) => this.renderPolylines(map, maps)}
+                >
+                    
+                </GoogleMapReact>
+            </div>
+        );
+    }
+    renderPolylines(map, maps) {
+        let geodesicPolyline = new maps.Polyline({
+            path: [
+                { lat: 53.42728, lng: -6.24357 },
+                { lat: 43.681583, lng: -79.61146 }
+            ],
+            geodesic: true,
+            strokeColor: '#00a1e1',
+            strokeOpacity: 1.0,
+            strokeWeight: 4
+        });
+        geodesicPolyline.setMap(map);
+    }
 }
