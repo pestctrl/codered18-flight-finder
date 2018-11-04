fe50cd140a2be5dbab9f80325fe11c131aef68cc
Benson Chu <bensonchu457@gmail.com>
2018-11-04 08:13:50 -0600

    Dummy data from here

--- api/run.py
+++ api/run.py
@@ -4,12 +4,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=["POST"])
 def hello_world():
-    thisdict = get_cheapest_flight(request.form["depart"],
-                                       request.form["arrive"],
-                                       request.form["date1"],
-                                       request.form["date2"],
-                                       int(request.form["num_ppl"]))
+    # thisdict = get_cheapest_flight(request.form["depart"],
+    #                                    request.form["arrive"],
+    #                                    request.form["date1"],
+    #                                    request.form["date2"],
+    #                                    int(request.form["num_ppl"]))
+    thisdict = return {
+        "price": price,
+        "airports": ["IAH", "ATL"],
+        "url": "https://google.com"
+    }
     print(thisdict)
+
     return jsonify(thisdict)
 
 if __name__ == "__main__":
