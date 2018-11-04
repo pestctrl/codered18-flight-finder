2b05495880341eff50e67e7602988c388c02280d
Benson Chu <bensonchu457@gmail.com>
2018-11-04 03:36:07 -0600

    Now a post request

--- api/run.py
+++ api/run.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 from scrape import get_cheapest_flight
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["POST"])
 def hello_world():
-   return get_cheapest_flight("Houston","Atlanta","11/15/2018", "11/25/2018", 4)
+    return get_cheapest_flight(request.form["depart"], request.form["arrive"], request.form["date1"], request.form["date2"], int(request.form["num_ppl"]))
 
 if __name__ == "__main__":
    app.run()
