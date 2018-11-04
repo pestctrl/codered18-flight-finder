971b4093fc206f4dee8363cac2d5215d8236a95c
Benson Chu <bensonchu457@gmail.com>
2018-11-04 08:24:00 -0600

    C# backend connected to flask

--- FlightFinder/Controllers/FlightsController.cs
+++ FlightFinder/Controllers/FlightsController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,14 +11,28 @@ namespace FlightFinder.Controllers
     public class FlightsController : Controller
     {
         [HttpPost("[action]")]
-        public JsonResult GetFlights(string start, string end)
+        public ActionResult GetFlights(string start, string end)
         {
-            return Json(new {
-                    success = true,
-                    airports = new [] {"IAH", "ATL"},
-                    price=113,
-                    link="https://google.com"
+            using (HttpClient client = new HttpClient())
+            {
+                var formContent = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("depart", start),
+                    new KeyValuePair<string, string>("arrive", end),
+                    new KeyValuePair<string, string>("date1", "11/15/2018"),
+                    new KeyValuePair<string, string>("date2", "11/25/2018"),
+                    new KeyValuePair<string, string>("num_ppl", "2")
                 });
+
+                var myHttpClient = new HttpClient();
+                var response = Task.Run(() => myHttpClient.PostAsync("http://35.231.94.254:5000/", formContent)).Result;
+                var content = Task.Run(() => response.Content.ReadAsStringAsync()).Result;
+                return new ContentResult
+                {
+                    Content = content,
+                    ContentType = "application/json"
+                };
+            }
         }
         
 
